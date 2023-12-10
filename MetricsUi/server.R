4c7511d46eb56d333fbcd2ce6cf09299b663ddd7
Federic23 <federico.rainaldi@brain-power.com>
2023-12-10 10:24:31 -0300

    Add Ui MVP

--- MetricsUi/server.R
+++ MetricsUi/server.R
@@ -3,33 +3,34 @@ source("modules/metric_groups.R")
 library(ggplot2)
 
 server <- function(input, output, session) {
+  shinyjs::useShinyjs()
   source("modules/route_page_control.R", local = TRUE)
   
-  #####################################################
   
-  ########CSV CHOOSER CONTROLLER
-  observe({
-    if (!is.null(input$file1)) {
-      file_path <- input$file1$datapath
-      cat("Uploaded File Path: ", file_path, "\n")
-    }
-  })
-  
-  ########CSV CHOOSER CONTROLLER
-  
+  ################################
+  ########DATE CHOOSER CONTROLLER
+  ################################
   filtered_data <- reactive({
     start_date <- input$startDate
     end_date <- input$endDate
     subset(df, date >= start_date & date <= end_date)
   })
   
-  ########DYNAMIC CONTROL
+  ################################
+  ########DYNAMIC CONTROL VIEW
+  ################################
   dynamicContent <- reactive({
     if (metric_content_visible()) {
+      Sys.sleep(2)
+      div(
+        div(class = "metric-group-div",
+            div(h5("Daily Average Total Result :"),
+                h3("6.2")),
+        ),
       fluidRow(
         lapply(metricsGroups, function(group) {
           div(class = "metric-group-div",
-              h4(group$groupName),
+              h4(group$groupName, group$value),
               lapply(split(group$metrics, (seq_along(group$metrics) - 1) %/% 2), function(pair) {
                 fluidRow(
                   lapply(pair, function(metric) {
@@ -38,52 +39,137 @@ server <- function(input, output, session) {
                 )
               })
           )
-        })
-      )
-      
-      # fluidRow(
-      #   lapply(metricsGroups, function(group) {
-      #     div(class = "metric-group-div",
-      #         h4(group$groupName),
-      #         lapply(split(group$metrics, (seq_along(group$metrics) - 1) %/% 2), function(pair) {
-      #           fluidRow(
-      #             lapply(pair, function(metric) {
-      #               column(class = "metric-group-divs", 6,
-      #                      h6(metric$name),
-      #                      numericInput(inputId = paste("input", metric$name, sep="_"), 
-      #                                   label = NULL, 
-      #                                   value = 0, 
-      #                                   min = 0, 
-      #                                   max = 1, 
-      #                                   step = 0.01))
-      #             })
-      #           )
-      #         })
-      #     )
-      #   })
-      # )
+        }), 
+      ))
     } else if (overall_metric_content_visible()){
-      div(renderPlot({
+      div(class = "plot", renderPlot({
         p <- ggplot(filtered_data(), aes(x = date, y = value)) +
-          geom_line() +
+          geom_line(linewidth = 1.5) +  # Use 'linewidth' for a thicker line
           labs(
-            title = "Line Plot of Group 1 Metrics",
+            title = "Overall rating per day",
             x = "Date",
             y = "Value"
           ) +
-          theme_minimal()
-        
+          theme_minimal(base_size = 14) +  # Increase the base font size
+          theme(
+            plot.title = element_text(size = 28),  # Larger title font
+            axis.title = element_text(size = 16),  # Larger axis title font
+            axis.text = element_text(size = 14)    # Larger axis text font
+          )
         p
       }),
       div(dateInput("startDate", "Select a start date:", value = "2023-10-15")),
       dateInput("endDate", "Select an end date:", value = "2023-10-22"))
       
+    } else if (weight_content_visible()){
+      div(generateDynamicInputs(metricsGroups),
+          actionButton(class = "metric-button", "CsvFormatButton", "Generate Metrics"))
     }
   })
   
   output$dynamicContent <- renderUI({
     dynamicContent()
   })
+  
+  ################################
+  ###################GENERATE INPUTS FOR WEIGHT
+  ################################
+  
+  generateDynamicInputs <- function(metricsGroups) {
+    input_list <- lapply(metricsGroups, function(group, group_idx) {
+      # Create a unique ID for the group numeric input
+      group_input_id <- paste("group_num_input", gsub("[^A-Za-z0-9]", "", group$groupName), sep="_")
+      total_metrics <- length(group$metrics)
+      div(class = "metric-group-div",
+          fluidRow(
+            column(1, h4(group$groupName)),
+            column(11, numericInput(inputId = group_input_id, 
+                                   label = NULL, 
+                                   value = 0.33, 
+                                   min = 0,
+                                   max = 1, 
+                                   step = 0.01), class = "parent-metric-input",)
+          ),
+            # Get the total number of metrics in the group
+          lapply(split(group$metrics, (seq_along(group$metrics) - 1) %/% 2), function(pair) {
+            fluidRow(
+              lapply(pair, function(metric) {
+                value <- if (total_metrics > 0) round(1 / total_metrics, 2) else 0 # Round to two decimal places
+                column(class = "metric-group-divs", 6,
+                       fluidRow(
+                         column(3, h6(metric$name)),
+                         column(9, numericInput(inputId = paste("input", gsub("[^A-Za-z0-9]", "", metric$name), sep="_"),
+                                                label = NULL, 
+                                                value = value,  # Value rounded to two decimal places
+                                                min = 0, 
+                                                max = 1, 
+                                                step = 0.01))
+                       ))
+              })
+            )
+          })
+      )
+      
+    })
+    do.call(tagList, input_list)
+  }
+  
+  observe({
+    lapply(metricsGroups, function(group) {
+      # Observer for group numeric input
+      group_num_input_id <- paste("group_num_input", gsub("[^A-Za-z0-9]", "", group$groupName), sep="_")
+      if (group_num_input_id %in% names(input)) {
+        observeEvent(input[[group_num_input_id]], {
+          # Handle the numeric input for the group
+          cat(paste("Group numeric input", group_num_input_id, "changed to", input[[group_num_input_id]], "\n"))
+        }, ignoreInit = TRUE)
+      }
+      
+      # Observers for metric numeric inputs
+      lapply(group$metrics, function(metric) {
+        metric_input_id <- paste("input", gsub("[^A-Za-z0-9]", "", metric$name), sep="_")
+        if (metric_input_id %in% names(input)) {
+          observeEvent(input[[metric_input_id]], {
+            # Handle the metric input
+            cat(paste("Metric input", metric_input_id, "changed to", input[[metric_input_id]], "\n"))
+          }, ignoreInit = TRUE)
+        }
+      })
+    })
+  })
+  ################################
+  ############################GENERATE CSV DATA 
+  ################################
+  
+  show_csv_data <- reactiveVal(FALSE)
+  
+  observeEvent(input$file1, {
+    # Print a message to the console when a file is uploaded
+    print(paste("File", input$file1$name, "has been uploaded."))
+    show_csv_data(TRUE)
+  })
+  
+  # Render the conditional UI for the CSV data display
+  output$csvDataDisplay <- renderUI({
+    if (show_csv_data()) {  # This will now correctly reference the reactive value
+      # Display the CSV data-related UI components
+      fluidRow(
+        column(class = "metric-group-divs", 3, h6("Size:", " 118KB")),
+        column(class = "metric-group-divs", 3, h6("Starting Date:", "14/10/23 12:42")),
+        column(class = "metric-group-divs", 3, h6("Finish Date: ", "15/10/23 15:31")),
+        column(class = "metric-group-divs", 3, h6("Row count: ", "84478"))
+      )
+    } else {
+      # Display placeholders or alternative content when CSV data isn't shown
+      fluidRow(
+        column(class = "metric-group-divs", 3, h6("Size: ", " -")),
+        column(class = "metric-group-divs", 3, h6("Starting Date: ", " -")),
+        column(class = "metric-group-divs", 3, h6("Finish Date: ", " -")),
+        column(class = "metric-group-divs", 3, h6("Row count: ", " -"))
+      )
+    }
+  })
+  
 }
 
 
--- MetricsUi/ui.R
+++ MetricsUi/ui.R
@@ -1,12 +1,16 @@
 library(shiny)
 library(shinythemes)
 library(ggplot2)
+library(shinyjs)
+library(shinycssloaders)
+
 
 source("ui_modules/variable_side_bar.R")
 
 
 
 ui <- fluidPage(
+  shinyjs::useShinyjs(),
   theme = shinytheme("paper"),
   includeCSS("styles.css"),
   
@@ -16,9 +20,12 @@ ui <- fluidPage(
            div(class = "title-panel",
                # titlePanel("Test 2 UI geoportales"),
                img(src = "logo.png", height = "250px", width = "auto")),
-           
+           div(class = "metric-group-div",
+               h5("Current CSV Data"),
+               uiOutput("csvDataDisplay")  # This is where the conditional UI will be displayed
+           ),
            div(
-             div(class = "metrics-panel", uiOutput("dynamicContent"),),
+             div(class = "metrics-panel", withSpinner(uiOutput("dynamicContent")),),
            )
     )
   )
